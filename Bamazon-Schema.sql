812574f02dffcd73a0fc0b5006ef656a7f889226
DABAYO <dbayo2000@yahoo.com>
2018-02-28 21:31:14 -0500

    still fixing things

--- Bamazon-Schema.sql
+++ Bamazon-Schema.sql
@@ -1,16 +1,44 @@
-drop database if exists bamazon_db;
-create database bamazon_db;
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
 
-use bamazon_db;
+USE bamazon_db;
 
-create table products(
-  item_id integer auto_increment not null,
-  product_name varchar(75) not null,
-  department_name varchar(50) not null,
-  price integer(10) default 0,
-  stock_quantity integer(10) default 0,
-  primary key(item_id)
+CREATE TABLE products(
+  item_id INTEGER AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(75) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price INTEGER(10) DEFAULT 0,
+  stock_quantity INTEGER(10) DEFAULT 0,
+  PRIMARY KEY(item_id)
 );
 
-select * from products;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Rocking Chair","Furniture", 80, 49);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Drone","Toys", 99, 76);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Bluetooth Speaker","Electronics", 75, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Wireless Headphones","Electronics", 150, 48);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Toaster","Kitchen", 24, 31);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Washing Machine","Aplliances", 360, 18);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Table Lamp","Furniture", 40, 103);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Dog Bed","Pet Supplies", 20, 14);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Soup Bowls, Set of 4","Kitchen", 16, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Bath Towel","Linens", 8, 34);
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,18 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var table = require("cli-table");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const Table = require('cli-table');
 
-// create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazon_db"
-});
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: '1234',
+    database: 'bamazon_db'
+  });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
@@ -17,30 +20,30 @@ connection.connect(function(err) {
   displayProducts();
 });
 
-// test sql connection
-// console.log ("connected as id" + connection.threadid); 
+// // test sql connection
+// console.log ('connected as id' + connection.threadid); 
 
 
 function displayProducts() {
-  //show all ids, names, and products from database.
+  //show all products from database.
   connection.query('SELECT * FROM products', function(error, response) {
       if (error) { console.log(error) };
       //New instance of our constructor
-      var theDisplayTable = new table({
+      const productTable = new Table({
           //declare the value categories
-          head: ['Item ID', 'Product Name', 'Category', 'Price', 'Quantity Available'],
+          head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity Available'],
           //set widths to scale
-          colWidths: [10, 30, 18, 10, 14]
+          colWidths: [10, 36, 24, 10, 20]
       });
       //for each row of the loop
       for (i = 0; i < response.length; i++) {
           //push data to table
-          theDisplayTable.push(
+          productTable.push(
               [response[i].item_id, response[i].product_name, response[i].department_name, response[i].price, response[i].stock_quantity]
           );
       }
       //log the completed table to console
-      console.log(theDisplayTable.toString());
+      console.log(productTable.toString());
       makePurchase();
   });
 };
@@ -49,26 +52,26 @@ function makePurchase() {
   //get item id and quantity desired from user.
   inquirer.prompt([
       {
-          name: "ID",
-          type: "input",
-          message: "What is the item number of the product you wish to purchase?"
+          name: 'ID',
+          type: 'input',
+          message: 'What is the item number of the product you wish to purchase?'
       }, 
       {
           name: 'Quantity',
           type: 'input',
-          message: "How many would you like to buy?"
+          message: 'How many would you like to buy?'
       },
 
   ]).then(function(answers) {
       var quantityDesired = answers.Quantity;
       var idDesired = answers.ID;
-      purchaseFromDatabase(idDesired, quantityDesired);
+      purchaseUpdateDatabase(idDesired, quantityDesired);
   });
 
 }; 
 
-function purchaseFromDatabase(ID, quantityNeeded) {
-  //check quantity of desired purchase. Minus quantity of the item_id from database if possible. Else inform user "Insufficient quantity!" 
+function purchaseUpdateDatabase(ID, quantityNeeded) {
+  //check quantity of desired purchase. Minus quantity of the item_id from database if possible. Else inform user 'Insufficient quantity!' 
   connection.query('SELECT * FROM products WHERE item_id = ' + ID, function(error, response) {
       if (error) { console.log(error) };
 
@@ -77,12 +80,12 @@ function purchaseFromDatabase(ID, quantityNeeded) {
           //calculate cost
           var totalCost = response[0].price * quantityNeeded;
           //inform user
-          console.log("Thank you for shopping with Bamazon!");
-          console.log("Your total cost for " + quantityNeeded + " " + response[0].product_name + " is " + totalCost + ".");
+        //   console.log('Thank you for shopping with Bamazon!');
+          console.log('Your total cost for ' + quantityNeeded + ' ' + response[0].product_name + ' is ' + totalCost + '.');
           //update database, minus purchased quantity
           connection.query('UPDATE products SET stock_quantity = stock_quantity - ' + quantityNeeded + ' WHERE item_id = ' + ID);
       } else {
-          console.log("Insufficient quantity!");
+          console.log('Insufficient quantity!');
       };
       displayproducts();
   });
